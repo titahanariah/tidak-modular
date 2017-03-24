a82aa5b9a79e981676f0f6982aef05f0779e38f4
titahanariah <titahanariah@users.noreply.github.com>
2017-03-24 23:55:26 +0700

    Add files via upload

---
+++ takmodular.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//variabel
+	int a, b, temp;
+	//proses membaca
+	cout <<"Masukkan nilai A:";
+	cin >> a;
+	
+	cout <<"Masukkan nilai B:";
+	cin >> b;
+	
+	//proses tukar
+	temp = b;
+	b = a;
+	a = temp;
+	
+	//proses tampil
+	cout <<"=========================="<<endl;
+	cout <<"Nilai A sekarang:"<<a<<endl;
+	cout <<"Nilai B sekarang:"<<b;
+	return 0;
+	}
